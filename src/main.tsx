0920830e53fccb57893205ca88f49c4d00000c33
春雷-李春雷 <chunlei.lcl@naturobot.com>
2023-04-23 21:01:01 +0800

    f

--- src/main.tsx
+++ src/main.tsx
@@ -1,127 +1,36 @@
 import './index.css'
 
-function createElement(type, props, ...children) {
-  return {
-    type,
-    props: {
-      ...props,
-      children: children.map(child => (typeof child === 'object' ? child : createTextElement(child)))
-    }
-  }
-}
-
-function createTextElement(text) {
-  return {
-    type: 'TEXT_ELEMENT',
-    props: {
-      nodeValue: text,
-      children: []
-    }
-  }
-}
+import createElement from './createElement'
 
 function createDom(fiber) {
   const dom = fiber.type == 'TEXT_ELEMENT' ? document.createTextNode('') : document.createElement(fiber.type)
 
-  updateDom(dom, {}, fiber.props)
+  const isProperty = key => key !== 'children'
+  Object.keys(fiber.props)
+    .filter(isProperty)
+    .forEach(name => {
+      dom[name] = fiber.props[name]
+    })
 
   return dom
 }
 
-const isEvent = key => key.startsWith('on')
-const isProperty = key => key !== 'children' && !isEvent(key)
-const isNew = (prev, next) => key => prev[key] !== next[key]
-const isGone = (prev, next) => key => !(key in next)
-
-function updateDom(dom, prevProps, nextProps) {
-  //Remove old or changed event listeners
-  Object.keys(prevProps)
-    .filter(isEvent)
-    .filter(key => !(key in nextProps) || isNew(prevProps, nextProps)(key))
-    .forEach(name => {
-      const eventType = name.toLowerCase().substring(2)
-      dom.removeEventListener(eventType, prevProps[name])
-    })
-
-  // Remove old properties
-  Object.keys(prevProps)
-    .filter(isProperty)
-    .filter(isGone(prevProps, nextProps))
-    .forEach(name => {
-      dom[name] = ''
-    })
-
-  // Set new or changed properties
-  Object.keys(nextProps)
-    .filter(isProperty)
-    .filter(isNew(prevProps, nextProps))
-    .forEach(name => {
-      dom[name] = nextProps[name]
-    })
-
-  // Add event listeners
-  Object.keys(nextProps)
-    .filter(isEvent)
-    .filter(isNew(prevProps, nextProps))
-    .forEach(name => {
-      const eventType = name.toLowerCase().substring(2)
-      dom.addEventListener(eventType, nextProps[name])
-    })
-}
-
-function commitRoot() {
-  deletions.forEach(commitWork)
-  commitWork(wipRoot.child)
-  currentRoot = wipRoot
-  wipRoot = null
-}
-
-function commitWork(fiber) {
-  if (!fiber) {
-    return
-  }
-
-  const domParent = fiber.parent.dom
-  if (fiber.effectTag === 'PLACEMENT' && fiber.dom != null) {
-    domParent.appendChild(fiber.dom)
-  } else if (fiber.effectTag === 'UPDATE' && fiber.dom != null) {
-    updateDom(fiber.dom, fiber.alternate.props, fiber.props)
-  } else if (fiber.effectTag === 'DELETION') {
-    domParent.removeChild(fiber.dom)
-  }
-
-  commitWork(fiber.child)
-  commitWork(fiber.sibling)
-}
-
 function render(element, container) {
-  wipRoot = {
+  // 每一个工作单元是 一个 fiber, 也就是一个 ReactElement
+  nextUnitOfWork = {
     dom: container,
     props: {
       children: [element]
-    },
-    alternate: currentRoot
+    }
   }
-  deletions = []
-  nextUnitOfWork = wipRoot
 }
 
-let nextUnitOfWork = null
-let currentRoot = null
-let wipRoot = null
-let deletions = null
-
 function workLoop(deadline) {
-  let shouldYield = false
+  let shouldYield = false // 是否有剩余时间
   while (nextUnitOfWork && !shouldYield) {
     nextUnitOfWork = performUnitOfWork(nextUnitOfWork)
     shouldYield = deadline.timeRemaining() < 1
   }
-
-  if (!nextUnitOfWork && wipRoot) {
-    commitRoot()
-  }
-
   requestIdleCallback(workLoop)
 }
 
@@ -132,71 +41,51 @@ function performUnitOfWork(fiber) {
     fiber.dom = createDom(fiber)
   }
 
-  const elements = fiber.props.children
-  reconcileChildren(fiber, elements)
+  if (fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom)
+  }
 
+  const elements = fiber.props.children
+  let index = 0
+  let prevSibling = null
+  while (index < elements.length) {
+    const element = elements[index]
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null
+    }
+
+    // 对子元素创建 fiber, 并添加 '父子关系 or 兄弟关系'
+    if (index === 0) {
+      fiber.child = newFiber
+    } else {
+      prevSibling.sibling = newFiber
+    }
+    prevSibling = newFiber
+    index++
+  }
+
+  // 处理第一个子元素
   if (fiber.child) {
     return fiber.child
   }
+
+  // 从第二个元素开始处理  (相当于不断地入栈出栈)
   let nextFiber = fiber
   while (nextFiber) {
+    // 同级向后
     if (nextFiber.sibling) {
       return nextFiber.sibling
     }
+
+    // 最后一个子元素, 回到上级, 之后在处理上一级的下一个同级元素
     nextFiber = nextFiber.parent
   }
 }
 
-function reconcileChildren(wipFiber, elements) {
-  let index = 0
-  let oldFiber = wipFiber.alternate && wipFiber.alternate.child
-  let prevSibling = null
-
-  while (index < elements.length || oldFiber != null) {
-    const element = elements[index]
-    let newFiber = null
-
-    const sameType = oldFiber && element && element.type == oldFiber.type
-
-    if (sameType) {
-      newFiber = {
-        type: oldFiber.type,
-        props: element.props,
-        dom: oldFiber.dom,
-        parent: wipFiber,
-        alternate: oldFiber,
-        effectTag: 'UPDATE'
-      }
-    }
-    if (element && !sameType) {
-      newFiber = {
-        type: element.type,
-        props: element.props,
-        dom: null,
-        parent: wipFiber,
-        alternate: null,
-        effectTag: 'PLACEMENT'
-      }
-    }
-    if (oldFiber && !sameType) {
-      oldFiber.effectTag = 'DELETION'
-      deletions.push(oldFiber)
-    }
-
-    if (oldFiber) {
-      oldFiber = oldFiber.sibling
-    }
-
-    if (index === 0) {
-      wipFiber.child = newFiber
-    } else if (element) {
-      prevSibling.sibling = newFiber
-    }
-
-    prevSibling = newFiber
-    index++
-  }
-}
+let nextUnitOfWork = null // fiber
 
 const Didact = {
   createElement,
@@ -212,10 +101,14 @@ const updateValue = e => {
 
 const rerender = value => {
   const element = (
-    <div>
+    <section className="section">
       <input onInput={updateValue} value={value} />
-      <h2>Hello {value}</h2>
-    </div>
+      {/* <h2>{value}</h2>
+      <section>
+        <header>header</header>
+        <main>main</main>
+      </section> */}
+    </section>
   )
 
   console.log(element)
