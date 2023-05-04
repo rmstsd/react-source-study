c541ca5e7c9f89eb3d1e1fc819d303f5620a7468
春雷-李春雷 <chunlei.lcl@naturobot.com>
2023-05-04 20:40:50 +0800

    Step VI

--- src/main.tsx
+++ src/main.tsx
@@ -15,22 +15,50 @@ function createDom(fiber) {
   return dom
 }
 
+function commitRoot() {
+  commitWork(wipRoot.child)
+  wipRoot = null
+}
+
+function commitWork(fiber) {
+  if (!fiber) {
+    return
+  }
+
+  const domParent = fiber.parent.dom
+  domParent.appendChild(fiber.dom)
+
+  commitWork(fiber.child)
+  commitWork(fiber.sibling)
+}
+
 function render(element, container) {
+  console.log('render')
   // 每一个工作单元是 一个 fiber, 也就是一个 ReactElement
-  nextUnitOfWork = {
+  wipRoot = {
     dom: container,
     props: {
       children: [element]
     }
   }
+
+  nextUnitOfWork = wipRoot
 }
 
+let wipRoot = null
+let nextUnitOfWork = null // fiber
+// Step VI
 function workLoop(deadline) {
   let shouldYield = false // 是否有剩余时间
   while (nextUnitOfWork && !shouldYield) {
     nextUnitOfWork = performUnitOfWork(nextUnitOfWork)
     shouldYield = deadline.timeRemaining() < 1
   }
+
+  if (!nextUnitOfWork && wipRoot) {
+    commitRoot()
+  }
+
   requestIdleCallback(workLoop)
 }
 
@@ -39,10 +67,6 @@ requestIdleCallback(workLoop)
 function performUnitOfWork(fiber) {
   if (!fiber.dom) {
     fiber.dom = createDom(fiber)
-  }
-
-  if (fiber.parent) {
-    fiber.parent.dom.appendChild(fiber.dom)
   }
 
   const elements = fiber.props.children
@@ -84,8 +108,6 @@ function performUnitOfWork(fiber) {
     nextFiber = nextFiber.parent
   }
 }
-
-let nextUnitOfWork = null // fiber
 
 const Didact = {
   createElement,
