7ee61c43ef55bdcc550ae5a47078cb8b88770706
春雷-李春雷 <chunlei.lcl@naturobot.com>
2023-05-06 14:17:35 +0800

    butiful dir code

--- src/lib/render.ts
+++ src/lib/render.ts
@@ -1,53 +1,5 @@
 import createElement from './createElement'
-
-function createDom(fiber) {
-  const dom = fiber.type == 'TEXT_ELEMENT' ? document.createTextNode('') : document.createElement(fiber.type)
-
-  updateDom(dom, {}, fiber.props)
-
-  return dom
-}
-
-const isEvent = key => key.startsWith('on')
-const isProperty = key => key !== 'children' && !isEvent(key)
-const isNew = (prev, next) => key => prev[key] !== next[key]
-const isGone = (prev, next) => key => !(key in next)
-
-function updateDom(dom, prevProps, nextProps) {
-  // 移除旧的事件监听
-  Object.keys(prevProps)
-    .filter(isEvent)
-    .filter(key => !(key in nextProps) || isNew(prevProps, nextProps)(key))
-    .forEach(name => {
-      const eventType = name.toLowerCase().substring(2)
-      dom.removeEventListener(eventType, prevProps[name])
-    })
-
-  // 移除旧的属性
-  Object.keys(prevProps)
-    .filter(isProperty)
-    .filter(isGone(prevProps, nextProps))
-    .forEach(name => {
-      dom[name] = ''
-    })
-
-  // 添加新属性
-  Object.keys(nextProps)
-    .filter(isProperty)
-    .filter(isNew(prevProps, nextProps))
-    .forEach(name => {
-      dom[name] = nextProps[name]
-    })
-
-  // 添加事件监听
-  Object.keys(nextProps)
-    .filter(isEvent)
-    .filter(isNew(prevProps, nextProps))
-    .forEach(name => {
-      const eventType = name.toLowerCase().substring(2)
-      dom.addEventListener(eventType, nextProps[name])
-    })
-}
+import { createDom, updateDom } from './domHepler'
 
 function commitRoot() {
   deletions.forEach(commitWork)
@@ -94,7 +46,6 @@ let currentRoot = null
 let nextUnitOfWork = null // fiber
 let deletions = null
 
-// Step VI
 function workLoop(deadline) {
   let shouldYield = false // 是否有剩余时间
   while (nextUnitOfWork && !shouldYield) {
@@ -138,6 +89,7 @@ function performUnitOfWork(fiber) {
   }
 }
 
+// 构建 fiber 链表 + diff
 function reconcileChildren(wipFiber, elements) {
   let index = 0
 
@@ -182,13 +134,6 @@ function reconcileChildren(wipFiber, elements) {
     if (oldFiber) {
       oldFiber = oldFiber.sibling
     }
-
-    // const newFiber = {
-    //   type: element.type,
-    //   props: element.props,
-    //   parent: wipFiber,
-    //   dom: null
-    // }
 
     // 对子元素创建 fiber, 并添加 '父子关系 or 兄弟关系'
     if (index === 0) {
