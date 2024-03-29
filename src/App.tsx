b0513132511bb67a42268b1c14187e280bf5009d
rmst <2864617610@qq.com>
2024-03-29 13:39:12 +0800

    20 批量执行 action

--- src/App.tsx
+++ src/App.tsx
@@ -4,67 +4,28 @@ function Foo() {
   console.log('Foo render')
 
   const [count, setCount] = React.useState(1)
-  const [count2, setCount2] = React.useState(10)
 
   return (
     <div>
       <button
         onClick={() => {
-          setCount(v => v + 1)
+          setCount(count + 1)
         }}
       >
         count {count}
-      </button>
-      <button
-        onClick={() => {
-          setCount2(v => v + 1)
-        }}
-      >
-        count2 {count2}
       </button>
     </div>
   )
 }
 
-let countBar = 1
-function Bar() {
-  console.log('Bar render')
-
-  const up = React.update()
-
-  return (
-    <button
-      onClick={() => {
-        countBar++
-        up()
-      }}
-    >
-      bar {countBar}
-    </button>
-  )
-}
-
-let countApp = 1
 function App() {
   console.log('App render')
 
-  const up = React.update()
-
   return (
     <header id="app">
-      <button
-        onClick={() => {
-          countApp++
-          up()
-        }}
-      >
-        {countApp}
-      </button>
-
       <hr />
 
       <Foo />
-      {/* <Bar /> */}
     </header>
   )
 }
